8221bfed0fd9619a399be67e1c2c92025e4f7766
landarenko0 <landarenko_nikita@mail.ru>
2023-06-12 15:34:47 +0500

    Added username to review. Now you need to specify username to register.

--- app/src/main/java/com/example/ratefilm/Registration.java
+++ app/src/main/java/com/example/ratefilm/Registration.java
@@ -16,13 +16,18 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Registration extends AppCompatActivity {
 
     private RegistrationBinding binding;
+    private EditText username_et;
     private EditText login_et;
     private EditText password_et;
     private FirebaseAuth mAuth;
+    private DatabaseReference database;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,9 +42,11 @@ public class Registration extends AppCompatActivity {
     private void init() {
         binding = RegistrationBinding.inflate(getLayoutInflater());
 
+        username_et = binding.etUsername;
         login_et = binding.registerLogin;
         password_et = binding.registerPassword;
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
 
         binding.register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,26 +57,43 @@ public class Registration extends AppCompatActivity {
     }
 
     private void register() {
+        String username = username_et.getText().toString();
         String login = login_et.getText().toString();
         String password = password_et.getText().toString();
 
-        if (login.equals("") || password.equals("")) {
+        if (username.equals("") || login.equals("") || password.equals("")) {
             Toast.makeText(this, "Пожалуйста, заполните все поля", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        mAuth.createUserWithEmailAndPassword(login, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        database.child("Users").get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
             @Override
-            public void onSuccess(AuthResult authResult) {
-                Toast.makeText(getApplicationContext(), "Вы успешно зарегистрировались", Toast.LENGTH_SHORT).show();
+            public void onSuccess(DataSnapshot dataSnapshot) {
 
-                Intent intent = new Intent(Registration.this, MainActivity.class);
-                startActivity(intent);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), "Произошла ошибка. Попробуйте снова", Toast.LENGTH_LONG).show();
+                if (!dataSnapshot.hasChild(username)) {
+                    mAuth.createUserWithEmailAndPassword(login, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            User user = new User(username);
+
+                            String[] tmp = login.split("@");
+
+                            database.child("Users").child(tmp[0]).setValue(user);
+
+                            Toast.makeText(getApplicationContext(), "Вы успешно зарегистрировались", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(Registration.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Произошла ошибка. Попробуйте снова", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Пользователь с таким именем уже существует", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/ratefilm/User.java
+++ app/src/main/java/com/example/ratefilm/User.java
@@ -5,9 +5,14 @@ import java.util.List;
 public class User {
     private List<Review> reviews;
     private String username;
+    private List<FilmToDB> likedFilms;
 
     public User() {
 
+    }
+
+    public User(String username) {
+        this.username = username;
     }
 
     public User(List<Review> reviews) {
@@ -36,5 +41,13 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public List<FilmToDB> getLikedFilms() {
+        return likedFilms;
+    }
+
+    public void setLikedFilms(List<FilmToDB> likedFilms) {
+        this.likedFilms = likedFilms;
     }
 }
