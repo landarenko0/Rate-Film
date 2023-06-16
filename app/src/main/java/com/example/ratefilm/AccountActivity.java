798387553da08881e279a5b07cd52cade62fec3a
landarenko0 <landarenko_nikita@mail.ru>
2023-06-16 22:44:56 +0500

    Added account activity

---
+++ app/src/main/java/com/example/ratefilm/AccountActivity.java
@@ -0,0 +1,156 @@
+package com.example.ratefilm;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ratefilm.databinding.AccountLayoutBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class AccountActivity extends AppCompatActivity {
+
+    private AccountLayoutBinding binding;
+    private User user;
+    private ArrayList<FilmToDB> likedFilms;
+    private ArrayList<FilmToDB> filmsWithReview;
+    private DatabaseReference database;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        init();
+
+        setContentView(binding.getRoot());
+
+        DownloadUserLikedFilmsThread thread = new DownloadUserLikedFilmsThread();
+        thread.start();
+    }
+
+    private void init() {
+        binding = AccountLayoutBinding.inflate(getLayoutInflater());
+
+        database = FirebaseDatabase.getInstance().getReference();
+
+        likedFilms = new ArrayList<>();
+        filmsWithReview = new ArrayList<>();
+
+        Gson gson = new Gson();
+
+        user = gson.fromJson(getIntent().getStringExtra("userJson"), User.class);
+
+        binding.accountUsername.setText(user.getUsername());
+        binding.accountEmail.setText(user.getEmail());
+    }
+
+    private class DownloadUserLikedFilmsThread extends Thread {
+
+        @Override
+        public void run() {
+            super.run();
+
+            database.child("Users").child("likedFilms").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DataSnapshot snapshot = task.getResult();
+
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            FilmToDB film = dataSnapshot.getValue(FilmToDB.class);
+
+                            likedFilms.add(film);
+                        }
+
+                        DownloadPosterThread thread = new DownloadPosterThread(likedFilms, binding.likedFilms);
+                        thread.start();
+
+                        FilmsListAdapter adapter = new FilmsListAdapter(likedFilms, user, AccountActivity.this);
+                        binding.likedFilms.setAdapter(adapter);
+                        binding.likedFilms.setLayoutManager(new LinearLayoutManager(AccountActivity.this, LinearLayoutManager.HORIZONTAL, false));
+                    }
+                }
+            });
+
+            database.child("Users").child("filmsWithReview").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DataSnapshot snapshot = task.getResult();
+
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            FilmToDB film = dataSnapshot.getValue(FilmToDB.class);
+
+                            filmsWithReview.add(film);
+                        }
+
+                        DownloadPosterThread thread = new DownloadPosterThread(filmsWithReview, binding.filmsWithReview);
+                        thread.start();
+
+                        FilmsListAdapter adapter = new FilmsListAdapter(filmsWithReview, user, AccountActivity.this);
+                        binding.filmsWithReview.setAdapter(adapter);
+                        binding.filmsWithReview.setLayoutManager(new LinearLayoutManager(AccountActivity.this, LinearLayoutManager.HORIZONTAL, false));
+                    }
+                }
+            });
+        }
+    }
+
+    private class DownloadPosterThread extends Thread {
+
+        private final ArrayList<FilmToDB> films;
+        private final RecyclerView recyclerView;
+
+        DownloadPosterThread(ArrayList<FilmToDB> films, RecyclerView recyclerView) {
+            this.films = films;
+            this.recyclerView = recyclerView;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            for (int i = 0; i < films.size(); i++) {
+                FilmToDB film = films.get(i);
+
+                try {
+                    URL url = new URL(film.getPosterUrl());
+                    InputStream stream = (InputStream) url.getContent();
+
+                    if (stream != null) {
+                        film.setBitmap(BitmapFactory.decodeStream(stream));
+
+                        assert recyclerView.getAdapter() != null;
+
+                        int finalI = i;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                recyclerView.getAdapter().notifyItemChanged(finalI);
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Произошла ошибка при загрузке изображения", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
